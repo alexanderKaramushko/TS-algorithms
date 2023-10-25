375ce2fc2ac717bde765aea757295152f11300ee
1 <2>
2023-10-25 21:53:54 +0400

    add deque structure, solve task

---
+++ src/structures/deque.ts
@@ -0,0 +1,35 @@
+export class Deque<T> {
+  constructor(private items: T[]) { }
+
+  enqueueFirst(item: T) {
+    this.items = [item, ...this.items];
+  }
+
+  enqueueLast(item: T) {
+    this.items.push(item);
+  }
+
+  dequeueFirst() {
+    const last = this.items.shift();
+
+    return last;
+  }
+
+  dequeueLast() {
+    const last = this.items.pop();
+
+    return last;
+  }
+
+  peekFirst() {
+    return this.items[0];
+  }
+
+  peekLast() {
+    return this.items.at(this.items.length - 1);
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
---
+++ src/structures/tasks/orderQueue.ts
@@ -0,0 +1,43 @@
+/**
+ * @see {@link https://informatics.msk.ru/mod/statements/view.php?chapterid=112984#1}
+ */
+
+import { Deque } from "../deque";
+
+/**
+ * @description
+ * 
+ * "+" поставить гоблина в конец очереди
+ * "*" поставить гоблина в середину очереди (за центром, если кол-во в очереди нечетное)
+ * "-" завести текущего гоблина к шаману
+ */
+type Operation = {
+  type: '+' | '*' | '-';
+  value?: number;
+}
+
+/**
+ * @returns порядок гоблинов, которые зашли к шаману
+ */
+export function orderQueue(): (operations: Operation) => void {
+  const head = new Deque<number>([]);
+  const tail = new Deque<number>([]);
+
+  return (operation: Operation) => {
+    if (operation.type === '-') {
+      console.log(head.dequeueFirst())
+    }
+
+    if (operation.type === '*') {
+      tail.enqueueFirst(operation.value as number);
+    }
+
+    if (operation.type === '+') {
+      tail.enqueueLast(operation.value as number);
+    }
+
+    while (head.size() < tail.size()) {
+      head.enqueueLast(tail.dequeueFirst() as number);
+    }
+  }
+}
