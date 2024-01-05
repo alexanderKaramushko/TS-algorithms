b63e13fd8b909a6de048b4cf111c5aacaa8bb5c8
1 <2>
2024-01-05 20:57:59 +0400

    add linked queue

---
+++ src/structures/linked-queue.ts
@@ -0,0 +1,46 @@
+import { ListNode, NodeModel } from "./linked";
+
+/**
+ * @description Реализация очереди на основе связного списка
+ */
+export class LinkedQueue<T> {
+  private _size = 0
+  private front: NodeModel<T> | null = null
+  private rear: NodeModel<T> | null = null
+
+  enqueue(item: T) {
+    this._size += 1;
+
+    const node = new ListNode(item);
+
+    if (this.rear === null) {
+      this.front = node;
+      this.rear = node;
+    } else {
+      this.rear.next = node;
+      this.rear = node;
+    }
+  }
+
+  dequeue() {
+    if (this.size() === 0) {
+      throw new Error('Очередь пуста!');
+    }
+
+    this._size -= 1;
+
+    const temp = this.front?.value;
+
+    if (this.front === null) {
+      this.rear = null;
+    } else {
+      this.front = this.front.next;
+    }
+
+    return temp;
+  }
+
+  size() {
+    return this._size;
+  }
+};
