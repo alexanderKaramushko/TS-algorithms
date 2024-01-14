4cb0928a2fd52d486797de698f5b457f46697ea4
1 <2>
2024-01-14 16:12:12 +0400

    solve missing numbers via hash table

---
+++ src/structures/tasks/hash/missing-number.ts
@@ -0,0 +1,18 @@
+/**
+ * @see {@link https://leetcode.com/explore/featured/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4602}
+ * 
+ * ACCEPTED.
+ */
+export function missingNumber(numbers: number[]): number | null {
+  const set = new Set(numbers);
+  let missing = null;
+
+  for (let index = 0; index <= numbers.length; index++) {
+    if (!set.has(index)) {
+      missing = index;
+      break;
+    }
+  }
+
+  return missing;
+}
