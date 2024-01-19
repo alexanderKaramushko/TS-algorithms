2680885b696803fa78634c01ef344bfc1b5e1b85
1 <2>
2024-01-19 14:53:23 +0400

    olve tree target path sum iteratively

---
+++ src/structures/tasks/tree/path-sum-iterative.ts
@@ -0,0 +1,32 @@
+import { Stack } from "../../stack";
+import { BinaryTree } from "../../tree";
+
+/**
+ * @see {@link https://leetcode.com/problems/path-sum/description/}
+ *
+ *
+ * @description
+ */
+export function pathSumIterative(tree: BinaryTree<number>, targetSum: number) {
+  const stack = new Stack<[BinaryTree<number>, number]>([[tree, 0]]);
+
+  while (stack.size() > 0) {
+    const [node, curr = 0] = stack.pop() ?? [];
+
+    if (!node?.left && !node?.right) {
+      if (curr + (node?.value ?? 0) === targetSum) {
+        return true;
+      }
+    }
+
+    if (node?.left) {
+      stack.push([node.left, curr + node.value]);
+    }
+
+    if (node?.right) {
+      stack.push([node.right, curr + node.value]);
+    }
+  }
+
+  return false;
+}
