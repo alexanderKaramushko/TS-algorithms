c851c59d40f2510d279ac8c209bf67360fd82c8a
1 <2>
2024-02-26 21:58:20 +0400

    add binary templates

---
+++ src/algorithms/binary/binary.ts
@@ -0,0 +1,57 @@
+/**
+ * @description Шаблон бинарного поиска
+ */
+export function binary(arr: number[], target: number) {
+  let left = 0;
+  let right = arr.length;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] > target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
+
+/**
+ * @description Поиск крайнего левого индекса при дубликатах
+ */
+export function binaryLeft(arr: number[], target: number) {
+  let left = 0;
+  let right = arr.length;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] >= target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
+
+/**
+ * @description Поиск крайнего правого индекса при дубликатах
+ */
+export function binaryRight(arr: number[], target: number) {
+  let left = 0;
+  let right = arr.length;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] > target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
+
