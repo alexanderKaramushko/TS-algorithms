61b2f73698f5c714d81fb174072f10ce9b7b0b26
1 <2>
2024-03-24 16:18:47 +0400

    solve house robber via bottom-up DP

--- src/algorithms/dynamic/house-robber.ts
+++ src/algorithms/dynamic/house-robber.ts
@@ -1,7 +1,5 @@
 /**
  * @see {@link https://leetcode.com/problems/house-robber/description}
- *
- * @description
  */
 export function houseRobber(nums: number[]) {
   const results: Record<number, number> = {};
@@ -32,4 +30,28 @@ export function houseRobber(nums: number[]) {
   }
 
   return dp(nums.length - 1);
+}
+
+/**
+ * @see {@link https://leetcode.com/problems/house-robber/description}
+ */
+export function houseRobberBottomUp(nums: number[]) {
+  if (nums.length == 1) {
+    return nums[0];
+  }
+
+  let n = nums.length;
+  let dp = new Array(n).fill(0);
+
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+
+  for (let index = 2; index < n; index++) {
+    dp[index] = Math.max(
+      Math.max(dp[index - 2], dp[index - 1]),
+      dp[index - 2] + nums[index]
+    );
+  }
+
+  return Math.max(...dp);
 }
