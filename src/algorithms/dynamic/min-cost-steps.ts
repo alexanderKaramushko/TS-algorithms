0614c599396fc13d18e4241ebe44b9ee1e10866d
1 <2>
2024-03-24 13:49:12 +0400

    solve min costs steps via bottom-up DP

--- src/algorithms/dynamic/min-cost-steps.ts
+++ src/algorithms/dynamic/min-cost-steps.ts
@@ -41,3 +41,19 @@ export function minCostSteps(cost: number[]) {
 
   return dp(cost.length);
 }
+
+/**
+ * @see {@link https://leetcode.com/problems/min-cost-climbing-stairs/description}
+ * 
+ * @description Bottom-up подход.
+ */
+export function minCostStepsBottomUp(cost: number[]) {
+  let n = cost.length;
+  let dp = new Array(n + 1).fill(0);
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  }
+
+  return dp[n];
+}
